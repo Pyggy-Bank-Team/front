c55b5afba5a6a02e4bdc9ddfd62f753223977470
Denis Makarenko <denis-makarenko@outlook.com>
2020-06-27 09:51:02 +0300

    Rename solution and add a service

---
+++ scr/PiggyBank.WebSite/Models/Services/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PiggyBank.WebSite.Models.Services
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}

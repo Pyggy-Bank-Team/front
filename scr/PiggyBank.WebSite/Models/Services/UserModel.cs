8af675fc7ee13460637b63957fd5bcc49373a36b
Denis Makarenko <denis@bridgerpay.com>
2020-07-06 21:48:54 +0300

    Remove Blazor Server app

--- scr/PiggyBank.WebSite/Models/Services/UserModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PiggyBank.WebSite.Models.Services
-{
-    public class UserModel
-    {
-        public string UserName { get; set; }
-
-        public string Password { get; set; }
-    }
-}

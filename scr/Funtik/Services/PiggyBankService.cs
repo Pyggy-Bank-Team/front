815c8786156b787dd46aae9c3d823ae84099b984
Denis Makarenko <denis@bridgerpay.com>
2020-07-07 21:28:23 +0300

    Add a partinal class

--- scr/Funtik/Services/PiggyBankService.cs
+++ scr/Funtik/Services/PiggyBankService.cs
@@ -1,7 +1,12 @@
+using System.Net.Http;
+
 namespace Funtik.Services
 {
-    public class PiggyBankService
+    public partial class PiggyBankService
     {
-        
+        private readonly HttpClient _client;
+
+        public PiggyBankService(HttpClient client)
+            => _client = client;
     }
 }

1a43bed2b9553cfb951f078f55f722a1b75fe5ef
Denis Makarenko <denis-makarenko@outlook.com>
2021-03-08 19:26:40 +0300

    PB-47 Added switch languges

--- scr/Funtik/wwwroot/js/logic.js
+++ scr/Funtik/wwwroot/js/logic.js
@@ -1,11 +1,27 @@
-function showMenuLanguges(event){
-    console.log('hello');
+function showLanguges(event){
     var languges = document.getElementById("h-dropdonw-languges");
     if(languges){
         if(languges.style.display === "none"){
-            languges.style.display = "inline";
+            languges.style.display = "block";
         }else{
             languges.style.display = "none";
         }
     }
+}
+
+function changeToRussian(){
+    document.getElementById('h-language-butt-content').innerText = "Русский";
+    document.getElementById('h-description').innerText = "Приложение для планирования бюджета";
+    document.getElementById('b-main-text').innerText = "Бесплатное приложение для планирования бюджета";
+    document.getElementById('b-question-text').innerText = "По любым вопросам:";
+    document.getElementById('b-follow-text').innerText = "Подпишитесь на нас:";
+    document.getElementById('b-links-cont-1200').className = "";
+}
+
+function changeToEnglish(){
+    document.getElementById('h-language-butt-content').innerText = "English";
+    document.getElementById('h-description').innerText = "Home budget planing platform";
+    document.getElementById('b-main-text').innerText = "Free and easy to use budget planning application";
+    document.getElementById('b-question-text').innerText = "For any questions:";
+    document.getElementById('b-follow-text').innerText = "Follow us:";
 }

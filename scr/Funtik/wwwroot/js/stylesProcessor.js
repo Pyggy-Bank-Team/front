84588aa4d91b2dc5d4a076d86c79fed1f65f5186
Denis Makarenko <denis-makarenko@outlook.com>
2021-03-15 20:09:43 +0300

    PB-47 Save some changes

--- scr/Funtik/wwwroot/js/stylesProcessor.js
+++ scr/Funtik/wwwroot/js/stylesProcessor.js
@@ -35,13 +35,9 @@ function setMobileStyles(){
     document.getElementById("b-img-winstore").className = "b-img-winstore-mobile";
     document.getElementById("b-app-links-container").className = "b-app-links-container-mobile";
     document.getElementById("app-links-cont").className = "b-links-cont-mobile";
-    document.getElementById("b-main-text").className = "b-main-text-mobile";
 
     document.getElementById("b-main-img-container-2").className = "";
     document.getElementById("b-main-img-container").className = "hidden";
-    document.getElementById("clouds").className = "hidden";  
-
-    document.getElementById("h-language-butt-content").className = "h-language-butt-content-mobile";
 }
 
 function set720Styles(){
@@ -58,17 +54,9 @@ function set720Styles(){
     document.getElementById("b-img-winstore").className = "b-img-winstore-720";
     document.getElementById("b-app-links-container").className = "b-app-links-container-720";
     document.getElementById("app-links-cont").className = "b-links-cont-720";
-    document.getElementById("b-mail-text").className = "b-mail-text-720";
-    document.getElementById("b-main-text").className = "b-main-text-720";
     document.getElementById("b-main-img-container-2").className = "hidden";
     document.getElementById("b-main-img-container").className = "b-main-img-cont-720";
     document.getElementById("b-main-img").className = "b-main-img-720";
-    document.getElementById("clouds").className = "";
-    document.getElementById("grp-clouds-1").classList.remove("b-clouds")
-    document.getElementById("grp-clouds-2").classList.remove("b-clouds")
-
-    document.getElementById("h-language-butt").className = "h-language-butt-720";
-    document.getElementById("h-language-butt-content").className = "h-language-butt-content-720";
 }
 
 function set960Styles(){
@@ -84,18 +72,10 @@ function set960Styles(){
     document.getElementById("b-img-appstore").className = "b-img-appstore-960";
     document.getElementById("b-img-winstore").className = "b-img-winstore-960";
     document.getElementById("app-links-cont").className = "b-links-cont-960";
-    document.getElementById("b-mail-text").className = "b-mail-text-960";
-    document.getElementById("b-main-text").className = "b-main-text-960";
     document.getElementById("b-main-img-container-2").className = "hidden";
     document.getElementById("b-main-img-container").className = "b-main-img-cont-960";
     document.getElementById("b-main-img").className = "b-main-img-960";
     document.getElementById('b-log-winstore').className = "b-log-winstore-960";
-    document.getElementById("clouds").className = "";
-    document.getElementById("grp-clouds-1").classList.remove("b-clouds")
-    document.getElementById("grp-clouds-2").classList.remove("b-clouds")
-
-    document.getElementById("h-language-butt").className = "h-language-butt-960";
-    document.getElementById("h-language-butt-content").className = "h-language-butt-content-960";
 }
 
 function set1200Styles(){
@@ -111,16 +91,8 @@ function set1200Styles(){
     document.getElementById("b-img-winstore").className = "b-img-winstore-1200";
     document.getElementById("b-app-links-container").className = "b-app-links-container-1200";
     document.getElementById("app-links-cont").className = "b-links-cont-1200";
-    document.getElementById("b-mail-text").className = "b-mail-text-1200";
-    document.getElementById("b-main-text").className = "b-main-text-1200";
     document.getElementById("b-main-img-container-2").className = "hidden";
     document.getElementById("b-main-img-container").className = "b-main-img-cont-1200";
     document.getElementById("b-main-img").className = "b-main-img-1200";
     document.getElementById('b-log-winstore').className = "b-log-winstore-1200";
-    document.getElementById("clouds").className = "";
-    document.getElementById("grp-clouds-1").classList.remove("b-clouds");
-    document.getElementById("grp-clouds-2").classList.remove("b-clouds");    
-
-    document.getElementById("h-language-butt").className = "h-language-butt-1200";
-    document.getElementById("h-language-butt-content").className = "h-language-butt-content-1200";
 }

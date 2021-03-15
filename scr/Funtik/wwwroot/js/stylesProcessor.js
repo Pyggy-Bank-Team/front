a72d26c9cb6634c9f6532841bacfd3f72c6447fb
Denis Makarenko <denis-makarenko@outlook.com>
2021-03-15 21:00:08 +0300

    PB-47 Some fix

--- scr/Funtik/wwwroot/js/stylesProcessor.js
+++ scr/Funtik/wwwroot/js/stylesProcessor.js
@@ -26,18 +26,6 @@ function setMobileStyles(){
     document.getElementById("f-email").className = "f-email-mobile";
     document.getElementById("f-logo").className = "f-logo-moblie";
     document.getElementById("f-app-name").className = "f-app-name-mobile";
-
-    document.getElementById("b-mail-text").className = "b-mail-text-mobile";
-    document.getElementById("b-follow-text").className = "b-contact-text-mobile";
-    document.getElementById("b-question-text").className = "b-contact-text-mobile";
-    document.getElementById('b-log-winstore').className = "b-log-winstore-mobile";
-    document.getElementById("b-img-appstore").className = "b-img-appstore-mobile";
-    document.getElementById("b-img-winstore").className = "b-img-winstore-mobile";
-    document.getElementById("b-app-links-container").className = "b-app-links-container-mobile";
-    document.getElementById("app-links-cont").className = "b-links-cont-mobile";
-
-    document.getElementById("b-main-img-container-2").className = "";
-    document.getElementById("b-main-img-container").className = "hidden";
 }
 
 function set720Styles(){
@@ -46,17 +34,6 @@ function set720Styles(){
     document.getElementById("f-email").className = "f-email-720";
     document.getElementById("f-logo").className = "f-logo-720";
     document.getElementById("f-app-name").className = "f-app-name-720";
-
-    document.getElementById("b-follow-text").className = "b-contact-text-1200";
-    document.getElementById("b-question-text").className = "b-contact-text-1200";
-    document.getElementById('b-log-winstore').className = "b-log-winstore-720";
-    document.getElementById("b-img-appstore").className = "b-img-appstore-720";
-    document.getElementById("b-img-winstore").className = "b-img-winstore-720";
-    document.getElementById("b-app-links-container").className = "b-app-links-container-720";
-    document.getElementById("app-links-cont").className = "b-links-cont-720";
-    document.getElementById("b-main-img-container-2").className = "hidden";
-    document.getElementById("b-main-img-container").className = "b-main-img-cont-720";
-    document.getElementById("b-main-img").className = "b-main-img-720";
 }
 
 function set960Styles(){
@@ -65,17 +42,6 @@ function set960Styles(){
     document.getElementById("f-email").className = "f-email-960";
     document.getElementById("f-logo").className = "f-logo-960";
     document.getElementById("f-app-name").className = "f-app-name-960";
-
-    document.getElementById("b-follow-text").className = "b-contact-text-1200";
-    document.getElementById("b-question-text").className = "b-contact-text-1200";
-    document.getElementById("b-app-links-container").className = "b-app-links-container-960";
-    document.getElementById("b-img-appstore").className = "b-img-appstore-960";
-    document.getElementById("b-img-winstore").className = "b-img-winstore-960";
-    document.getElementById("app-links-cont").className = "b-links-cont-960";
-    document.getElementById("b-main-img-container-2").className = "hidden";
-    document.getElementById("b-main-img-container").className = "b-main-img-cont-960";
-    document.getElementById("b-main-img").className = "b-main-img-960";
-    document.getElementById('b-log-winstore').className = "b-log-winstore-960";
 }
 
 function set1200Styles(){
@@ -84,15 +50,4 @@ function set1200Styles(){
     document.getElementById("f-email").className = "f-email-1200";
     document.getElementById("f-logo").className = "f-logo-1200";
     document.getElementById("f-app-name").className = "f-app-name-1200";
-
-    document.getElementById("b-follow-text").className = "b-contact-text-1200";
-    document.getElementById("b-question-text").className = "b-contact-text-1200";
-    document.getElementById("b-img-appstore").className = "b-img-appstore-1200";
-    document.getElementById("b-img-winstore").className = "b-img-winstore-1200";
-    document.getElementById("b-app-links-container").className = "b-app-links-container-1200";
-    document.getElementById("app-links-cont").className = "b-links-cont-1200";
-    document.getElementById("b-main-img-container-2").className = "hidden";
-    document.getElementById("b-main-img-container").className = "b-main-img-cont-1200";
-    document.getElementById("b-main-img").className = "b-main-img-1200";
-    document.getElementById('b-log-winstore').className = "b-log-winstore-1200";
 }
